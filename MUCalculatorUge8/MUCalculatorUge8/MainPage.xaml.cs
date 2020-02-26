ac2410f34572c353c37cca805708540cad9dc6de
AndreasIT <andr522f@edu.ucl.dk>
2020-02-26 12:37:06 +0100

    events, functionality and navigation has been added.

--- MUCalculatorUge8/MUCalculatorUge8/MainPage.xaml.cs
+++ MUCalculatorUge8/MUCalculatorUge8/MainPage.xaml.cs
@@ -16,6 +16,73 @@ namespace MUCalculatorUge8
         public MainPage()
         {
             InitializeComponent();
+            OnClear(null, null);
+        }
+
+        int curretState;
+        string mathOperator;
+        double firstNumber;
+        double secondNumber;
+
+        private void OnSelectNumber(object sender, EventArgs e)
+        {
+            
+            if(curretState <0)
+            {
+                curretState = -1;
+            }
+
+            resultText.Text = ((Button)sender).Text;
+            
+            if (curretState == 1)
+            {
+                firstNumber = Convert.ToDouble(((Button)sender).Text);
+            }
+            else
+            {
+                secondNumber = Convert.ToDouble(((Button)sender).Text);
+            }
+            
+        }
+
+        private void OnSelectoperator(object sender, EventArgs e)
+        {
+                curretState = -2;
+                mathOperator = ((Button)sender).Text;
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            curretState = 1;
+        }
+
+        private void OnCalculate(object sender, EventArgs e)
+        {
+            if (mathOperator == "+")
+            {
+                resultText.Text = Convert.ToString(firstNumber + secondNumber);
+            }
+            else if (mathOperator == "-")
+            {
+                resultText.Text = Convert.ToString(firstNumber - secondNumber);
+            }
+            else if (mathOperator == "X")
+            {
+                resultText.Text = Convert.ToString(firstNumber * secondNumber);
+            }
+            else if (mathOperator == "/")
+            {
+                resultText.Text = Convert.ToString(firstNumber / secondNumber);
+            }
+
+            curretState = -1;
+            firstNumber = Convert.ToDouble(resultText.Text);
+        }
+
+        private async void toPageTwo(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
         }
     }
 }
---
+++ MUCalculatorUge8/MUCalculatorUge8/Page2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MUCalculatorUge8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private async void toRootPage(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
